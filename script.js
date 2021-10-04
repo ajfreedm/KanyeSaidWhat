ce30d843e5d8c6201505d73ebb584ffb5ac8a9b3
Andrew Freedman <ajfreedm@gmail.com>
2021-10-04 19:52:21 -0400

    adding more structure

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
 const quotesDiv = document.querySelector("#quotesDiv");
+const result = document.getElementById('result');
+
+// referenced https://stackoverflow.com/questions/65117741/how-do-i-call-a-random-function-on-button-click for solution to randomize the query functions
 const randomFunc = [getKayneQuote, getFamousQuote];
-let author;
+
 
 function getAnswer(data) {
-  answer = data.currentTarget.value;
+  answer = data.currentTarget.getAttribute("data-answer");
   console.log(answer);
+  let actualAnswer = document.querySelector(".answer").id;
 
- 
- 
+  if (actualAnswer == answer) {
+    console.log("win");
+    result.innerText = "You answer is correct";
+    
+  } else {
+    console.log("lose");
+    result.innerText = "Your answer is incorrect. Try again";
+  }
 
+  randomFunc[Math.floor(Math.random() * randomFunc.length)]();
 }
 
 document.querySelector("#yes").addEventListener("click", getAnswer);
 document.querySelector("#no").addEventListener("click", getAnswer);
 
-document.querySelector("#getQuote").addEventListener("click", function () {
-  randomFunc[Math.floor(Math.random() * randomFunc.length)](); 
-});
+// referenced https://stackoverflow.com/questions/65117741/how-do-i-call-a-random-function-on-button-click for solution to randomize the query functions
+randomFunc[Math.floor(Math.random() * randomFunc.length)]();
+
+
+
+// I removed this so the page loads pre populated with the quote.
+// document.querySelector("#getQuote").addEventListener("click", function () {
+//   randomFunc[Math.floor(Math.random() * randomFunc.length)]();
+// });
 
 function getKayneQuote() {
   fetch("https://api.kanye.rest")
@@ -24,6 +41,7 @@ function getKayneQuote() {
     .then((kanyeData) => {
       console.log(kanyeData.quote);
       quotesDiv.innerText = kanyeData.quote;
+      quotesDiv.setAttribute("id", "yes");
     });
 }
 
@@ -31,8 +49,8 @@ function getFamousQuote() {
   fetch("https://type.fit/api/quotes")
     .then((response) => response.json())
     .then((famousData) => {
-      quotesDiv.innerText = famousData[Math.floor(Math.random() * 1000)].author;
-      //quotesDiv.innerText = famousData[Math.floor(Math.random() * 1000)].text;
-    
+     quotesDiv.innerText = famousData[Math.floor(Math.random() * 1000)].author;
+     quotesDiv.innerText = famousData[Math.floor(Math.random() * 1000)].text;
+      //quotesDiv.setAttribute("id", "no");
     });
 }
